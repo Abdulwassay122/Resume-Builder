f02a13433586abf617aeb387c5e7c8383d38605b
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-09-08 14:17:54 +0500

    Third step is half done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var _a;
+var _a, _b;
+// contact info toggle
 function toggleVisibility() {
     var element = document.getElementById('contact-info');
     if (element) {
@@ -12,4 +13,18 @@ function toggleVisibility() {
 }
 (_a = document.getElementById('Btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
     toggleVisibility();
+});
+// Cerate resume button 
+// let createResumeBtn = document.getElementById('createResumeBtn')
+// let exampleResume = document.getElementById("main-container")
+var element1 = document.getElementById('main-container');
+var element2 = document.getElementById('form-container');
+function toggleForm() {
+    console.log("1222");
+    element1.style.display = 'none';
+    element2.style.display = 'block';
+}
+(_b = document.getElementById('createResumeBtn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', function () {
+    toggleForm();
+    console.log("object");
 });
--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 
-
+// contact info toggle
 function toggleVisibility(): void {
     let element = document.getElementById('contact-info')
     if (element) {
@@ -12,4 +12,21 @@ function toggleVisibility(): void {
 }
 document.getElementById('Btn')?.addEventListener('click', () => {
     toggleVisibility();
+});
+
+
+// Cerate resume button 
+// let createResumeBtn = document.getElementById('createResumeBtn')
+// let exampleResume = document.getElementById("main-container")
+
+let element1: any   = document.getElementById('main-container')
+let element2: any   = document.getElementById('form-container')
+function toggleForm(){
+    console.log("1222")
+    element1.style.display ='none'
+    element2.style.display ='block'
+}
+document.getElementById('createResumeBtn')?.addEventListener('click', () => {
+    toggleForm();
+    console.log("object")
 });
